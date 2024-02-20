a9a301a13cece95b52441929c7fc5adac61bdc81
alex borous <alexborousas@gmail.com>
2024-02-20 17:12:03 +0200

    Remove unnecessary files and assets

--- example/android/app/src/main/kotlin/com/altafrazzaque/example/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.altafrazzaque.example
-
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-class MainActivity: FlutterActivity() {
-}
